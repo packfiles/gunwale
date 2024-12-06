109ce3cc978107d9b94cc436f962d1f990737ce0
Charlton Trezevant <ct@ctis.me>
2024-12-06 13:11:57 -0500

    add new smarterCSV config opts

--- lib/row_boat/helpers.rb
+++ lib/row_boat/helpers.rb
@@ -4,11 +4,13 @@ module RowBoat
   # @api private
   module Helpers
     CSV_OPTION_KEYS = %i[
+      auto_row_sep_chars
       chunk_size
       col_sep
       comment_regexp
       convert_values_to_numeric
       downcase_header
+      duplicate_header_suffix
       file_encoding
       force_simple_split
       force_utf8
@@ -16,20 +18,26 @@ module RowBoat
       invalid_byte_sequence
       keep_original_headers
       key_mapping
+      missing_header_prefix
       quote_char
       remove_empty_hashes
       remove_empty_values
       remove_unmapped_keys
       remove_values_matching
       remove_zero_values
+      required_headers
+      required_keys
       row_sep
+      silence_missing_keys
       skip_lines
+      strict
       strings_as_keys
       strip_chars_from_headers
       strip_whitespace
       user_provided_headers
       value_converters
       verbose
+      with_line_numbers
     ].freeze
 
     IMPORT_OPTION_KEYS = %i[
